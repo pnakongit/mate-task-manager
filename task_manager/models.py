01b4a2ed42ca80fcecd12cca99ffaf538f9b6164
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2023-11-06 20:48:23 +0200

    Add Position model to task_manager app

--- task_manager/models.py
+++ task_manager/models.py
@@ -3,3 +3,10 @@ from django.db import models
 
 class Team(models.Model):
     name = models.CharField(max_length=65, unique=True)
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=65, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
