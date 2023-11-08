8c1fdd593e1bb8f7b2c915a00fa25bff6581335a
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2023-11-08 21:03:49 +0200

    Register Worker model as User in Django admin in task_manage app

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from task_manager.models import Worker
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("team", "position")
+    fieldsets = UserAdmin.fieldsets + (
+        ("Team info", {"fields": ("team", "position")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
+        ("Team info", {"fields": ("team", "position")})
+    )
