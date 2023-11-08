eff304bd256d84b1219b464f27ca7e5aa8e88a77
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2023-11-08 15:38:26 +0200

    Add Task model to task_manager app

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -39,3 +40,54 @@ class Project(models.Model):
     name = models.CharField(max_length=65)
     description = models.CharField(max_length=255)
     teams = models.ManyToManyField(to=Team, related_name="teams")
+
+
+class Task(models.Model):
+    class PriorityChoices(models.IntegerChoices):
+        LOW = 1, "Low"
+        NORMAL = 2, "Normal"
+        HIGH = 3, "High"
+        BLOCK = 4, "Block"
+
+    name = models.CharField(max_length=65)
+    description = models.CharField(max_length=255)
+    deadline = models.DateField()
+    task_type = models.ForeignKey(
+        to=TaskType,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="tasks"
+    )
+    is_completed = models.BooleanField(default=False)
+    priority = models.PositiveIntegerField(
+        choices=PriorityChoices.choices,
+        default=PriorityChoices.LOW
+    )
+    creator = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="created_tasks"
+    )
+    assignees = models.ManyToManyField(
+        to=settings.AUTH_USER_MODEL,
+        related_name="assigned_tasks"
+    )
+    project = models.ForeignKey(
+        to=Project,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(to=Tag, related_name="tasks")
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["name"], name="name_idx"),
+            models.Index(fields=["description"], name="description_idx"),
+        ]
+
+    def __str__(self) -> str:
+        return f"{self.pk} {self.name}"
+
