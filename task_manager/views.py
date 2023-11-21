f1a8ab8179eca7f028c9b90fe113f16f2771b6a8
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2023-11-21 20:22:49 +0200

    Create IndexView

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+import datetime
+from typing import Any
 
-# Create your views here.
+from django.views.generic import TemplateView
+
+from task_manager.models import Task, Activity
+
+
+class IndexView(TemplateView):
+    number_of_last_tasks = 10
+    number_of_last_activity = 10
+    template_name = "task_manager/index.html"
+
+    def get_context_data(self, **kwargs) -> dict[str: Any]:
+        kwargs = super().get_context_data(**kwargs)
+        user_team = self.request.user.team
+
+        context = {
+            "last_tasks": Task.objects.filter(
+                project__teams=user_team
+            ).order_by("-created_time")[:self.number_of_last_tasks].prefetch_related("assignees"),
+            "last_activity": Activity.objects.filter(
+                task__project__teams=user_team
+            ).order_by("-created_time"),
+            "count_unfinished_tasks": Task.objects.filter(
+                project__teams=user_team, is_completed=False
+            ).count(),
+            "count_unassigned_tasks": Task.objects.filter(
+                project__teams=user_team, assignees__isnull=True,
+            ).count(),
+            "count_over_deadline_tasks": Task.objects.filter(
+                project__teams=user_team, deadline__gt=datetime.date.today()
+            ).count()
+        }
+
+        kwargs.update(context)
+
+        return kwargs
