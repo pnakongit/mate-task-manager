968da816dd429ba919d7917b84ea1253a5ba88a9
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2023-11-22 11:02:15 +0200

    Add url pattern 'login' with name 'login' to task_manager app

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
 
 from task_manager.views import IndexView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("login/", LoginView.as_view(), name="login")
 ]
 
 app_name = "task_manager"
