9f9dddfe8fad9ba15c48fb0cc12d754033cd5041
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2023-11-22 12:30:19 +0200

    Add url pattern 'logout' with name 'logout' to task_manager app

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,11 +1,12 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from task_manager.views import IndexView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
-    path("login/", LoginView.as_view(), name="login")
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout")
 ]
 
 app_name = "task_manager"
