5d4559276113b8588c3d2229abe946c37983ed1f
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2023-11-22 12:28:13 +0200

    Add variable LOGIN_REDIRECT_URL to setting.py

--- mate_task_manager/settings.py
+++ mate_task_manager/settings.py
@@ -99,6 +99,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "task_manager.Worker"
 
+LOGIN_REDIRECT_URL = "task_manager:index"
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
