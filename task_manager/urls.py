a86bf6344107d8917980281216d323b0827feef6
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2023-11-27 18:29:10 +0200

    Rename view from TaskList to TaskListView in urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,13 +1,13 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from task_manager.views import IndexView, TaskList
+from task_manager.views import IndexView, TaskListView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
-    path("tasks/", TaskList.as_view(), name="task_list")
+    path("tasks/", TaskListView.as_view(), name="task_list"),
 ]
 
 app_name = "task_manager"
