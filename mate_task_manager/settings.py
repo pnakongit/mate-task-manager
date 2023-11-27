f80d7c052bc3c15cca6ffa67faf0d936505ae613
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2023-11-27 15:59:07 +0200

    Add django-crispy-forms configurations to setting.py

--- mate_task_manager/settings.py
+++ mate_task_manager/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "crispy_forms",
+    "crispy_bootstrap4",
     "task_manager",
 ]
 
@@ -125,3 +127,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
