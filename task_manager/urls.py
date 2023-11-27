ca00df362db7e835984805203b263309fce7b372
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2023-11-27 16:08:48 +0200

    Add urlpattern 'tasks' with name 'task_list' to task_manager app

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from task_manager.views import IndexView
+from task_manager.views import IndexView, TaskList
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("login/", LoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(), name="logout")
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("tasks/", TaskList.as_view(), name="task_list")
 ]
 
 app_name = "task_manager"
