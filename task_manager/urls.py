0e1d1c83a83f879da1b0847ad4d7d35f7a24a89a
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2023-12-05 22:57:14 +0200

    Add urlpattern 'task' with name 'task_create'

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from task_manager.views import IndexView, TaskListView, TaskDetailView
+from task_manager.views import IndexView, TaskListView, TaskDetailView, TaskCreateView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
@@ -9,6 +9,7 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(), name="logout"),
     path("tasks/", TaskListView.as_view(), name="task_list"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task_detail"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task_create")
 ]
 
 app_name = "task_manager"
