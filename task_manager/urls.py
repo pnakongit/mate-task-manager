4ed4484e3d3e5a2bc9cf21889aa6dff9260f6e7a
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2023-12-06 13:08:43 +0200

    Add urlpattern tasks/<int:pk>/update with name task_update to urls

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from task_manager.views import IndexView, TaskListView, TaskDetailView, TaskCreateView
+from task_manager.views import (
+    IndexView,
+    TaskListView,
+    TaskDetailView,
+    TaskCreateView,
+    TaskUpdateView,
+)
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
@@ -9,7 +15,8 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(), name="logout"),
     path("tasks/", TaskListView.as_view(), name="task_list"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task_detail"),
-    path("tasks/create/", TaskCreateView.as_view(), name="task_create")
+    path("tasks/create/", TaskCreateView.as_view(), name="task_create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task_update"),
 ]
 
 app_name = "task_manager"
