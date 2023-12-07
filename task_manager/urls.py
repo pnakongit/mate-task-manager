721359a34fbf422ba882a9f6996254b9f23eaa02
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2023-12-07 22:02:20 +0200

    Add urlpattern projects/<int:pk>/delete with name project_delete to urls

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -11,7 +11,8 @@ from task_manager.views import (
     ProjectListView,
     ProjectDetailView,
     ProjectCreateView,
-    ProjectUpdateView
+    ProjectUpdateView,
+    ProjectDeleteView
 )
 
 urlpatterns = [
@@ -26,7 +27,8 @@ urlpatterns = [
     path("projects/", ProjectListView.as_view(), name="project_list"),
     path("projects/<int:pk>/", ProjectDetailView.as_view(), name="project_detail"),
     path("projects/create/", ProjectCreateView.as_view(), name="project_create"),
-    path("projects/<int:pk>/update/", ProjectUpdateView.as_view(), name="project_update")
+    path("projects/<int:pk>/update/", ProjectUpdateView.as_view(), name="project_update"),
+    path("projects/<int:pk>/delete/", ProjectDeleteView.as_view(), name="project_delete")
 ]
 
 app_name = "task_manager"
