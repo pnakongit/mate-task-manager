cd333741c3785f8f14c36c937939102f5ac5965d
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2023-12-20 20:48:31 +0200

    Replace TaskListView with TaskListFilterView in urls

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from task_manager.views import (IndexView,
-                                TaskListView,
+                                TaskListFilterView,
                                 TaskDetailView,
                                 TaskCreateView,
                                 TaskUpdateView,
@@ -36,7 +36,7 @@ urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
-    path("tasks/", TaskListView.as_view(), name="task_list"),
+    path("tasks/", TaskListFilterView.as_view(), name="task_list"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task_detail"),
     path("tasks/create/", TaskCreateView.as_view(), name="task_create"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task_update"),
