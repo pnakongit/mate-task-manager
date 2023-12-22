8f3111bdec46256e07dcf260a33de87e4c75796e
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2023-12-22 16:35:58 +0200

    Change permission check logic in get_queryset in QuerysetFilterMixin

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -3,11 +3,12 @@ from django.db.models import QuerySet
 
 class QuerysetFilterMixin:
     filter_parameter_name = None
+    permission_parameter = None
 
     def get_queryset(self) -> QuerySet:
         queryset = super().get_queryset()
         user = self.request.user
-        if not (user.is_superuser or user.has_perm("task_manager.can_view_task")):
+        if not (user.is_superuser or user.has_perm(self.permission_parameter)):
             lookup_parameter = {self.filter_parameter_name: user}
             return queryset.filter(**lookup_parameter)
         return queryset
