91aeab665b9620a483d8de9275096758079d1265
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2023-12-26 18:42:58 +0200

    Overide get_queryset method in WorkerListFilterView

--- task_manager/views.py
+++ task_manager/views.py
@@ -339,9 +339,24 @@ class TeamDeleteView(generic.DeleteView):
 
 
 class WorkerListFilterView(ListFilterView):
-    model = Worker
+    model = get_user_model()
     paginate_by = settings.DEFAULT_PAGINATE_BY
     filter_form = WorkerListFilter
+    permission_parameter = "task_manager.view_worker"
+
+    def get_queryset(self) -> QuerySet:
+        queryset = super().get_queryset()
+        user = self.request.user
+
+        if not (user.is_superuser or user.has_perm(self.permission_parameter)):
+            worker_by_project = queryset.filter(
+                team__projects__in=self.request.user.team.projects.all()
+            )
+            worker_by_team = queryset.filter(team=self.request.user.team)
+
+            return worker_by_project | worker_by_team
+
+        return queryset
 
 
 class WorkerDetailView(generic.DetailView):
