1a61bd19db4cc18aef4f61b9019772f666c756ad
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2023-12-27 16:45:38 +0200

    Create TaskPermissionRequiredMixin in mixins

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.db.models import QuerySet
+from django.shortcuts import get_object_or_404
 
 
 class QuerysetFilterMixin:
@@ -12,3 +14,19 @@ class QuerysetFilterMixin:
             lookup_parameter = {self.filter_parameter_name: user}
             return queryset.filter(**lookup_parameter)
         return queryset
+
+
+class TaskPermissionRequiredMixin(PermissionRequiredMixin):
+
+    def has_permission(self) -> bool:
+        perms = self.get_permission_required()
+
+        if self.request.user.has_perms(perms):
+            return True
+
+        task = get_object_or_404(
+            self.model,
+            **{self.pk_url_kwarg: self.kwargs.get(self.pk_url_kwarg)}
+        )
+
+        return task.project in self.request.user.team.projects.all()
