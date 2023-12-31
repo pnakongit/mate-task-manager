32705b486a41a2ecce4253c7d545f659b6177248
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2023-12-31 17:12:52 +0200

    Add ProjectManager to managers

--- task_manager/managers.py
+++ task_manager/managers.py
@@ -6,3 +6,10 @@ class TaskManager(Manager):
     def filter_by_user(self, user) -> QuerySet:
         queryset = self.get_queryset()
         return queryset.filter(project__teams__workers=user)
+
+
+class ProjectManager(Manager):
+
+    def filter_by_user(self, user) -> QuerySet:
+        queryset = self.get_queryset()
+        return queryset.filter(teams__workers=user)
