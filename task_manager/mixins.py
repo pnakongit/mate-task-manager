dcb960dae74381b2aa69fd96af2a12c41f85cea2
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-01-02 16:14:50 +0200

    Change get_queryset method using filter_by_user in QuerysetFilterMixin

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -4,15 +4,21 @@ from django.shortcuts import get_object_or_404
 
 
 class QuerysetFilterMixin:
-    filter_parameter_name = None
     permission_parameter = None
 
     def get_queryset(self) -> QuerySet:
-        queryset = super().get_queryset()
         user = self.request.user
-        if not (user.is_superuser or user.has_perm(self.permission_parameter)):
-            lookup_parameter = {self.filter_parameter_name: user}
-            return queryset.filter(**lookup_parameter)
+
+        queryset = self.model.objects.all()
+
+        if not user.has_perm(self.permission_parameter):
+            queryset = self.model.objects.filter_by_user(user=user)
+
+        filters = self.get_filters()
+
+        if filters:
+            return queryset.filter(filters)
+
         return queryset
 
 
