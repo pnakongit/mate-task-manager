dcaba10d5db8ee0a8bfd861e7086386878bb795c
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-01-04 21:41:31 +0200

    Add querysets.py with TaskQuerySet to task_manager app

---
+++ task_manager/querysets.py
@@ -0,0 +1,9 @@
+from django.db.models import QuerySet
+
+
+class TaskQuerySet(QuerySet):
+    def filter_by_user(self, user) -> QuerySet:
+        if user.has_perm("task_manager.view_task"):
+            return self.all()
+
+        return self.filter(project__teams__workers=user)
