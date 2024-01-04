8d0743929a962d3502f93b483dae4486a20b104c
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-01-04 21:58:06 +0200

    Add ProjectQuerySet to querysets

--- task_manager/querysets.py
+++ task_manager/querysets.py
@@ -7,3 +7,11 @@ class TaskQuerySet(QuerySet):
             return self.all()
 
         return self.filter(project__teams__workers=user)
+
+
+class ProjectQuerySet(QuerySet):
+
+    def filter_by_user(self, user) -> QuerySet:
+        if user.has_perm("task_manager.view_project"):
+            return self.all()
+        return self.filter(teams__workers=user)
