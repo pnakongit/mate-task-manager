d1997b1d486b6831eb5df289b79b63423a931c00
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-01-12 18:49:23 +0200

    Add LOGIN_URL with task_manager:login to settings.py

--- mate_task_manager/settings.py
+++ mate_task_manager/settings.py
@@ -102,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTH_USER_MODEL = "task_manager.Worker"
 
 LOGIN_REDIRECT_URL = "task_manager:index"
+LOGIN_URL = "task_manager:login"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
