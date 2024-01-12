64b3f6414e2dbd023ff12f6df18c2da04614d217
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-01-12 17:29:28 +0200

    Define queryset attribute in TeamUpdateView

--- task_manager/views.py
+++ task_manager/views.py
@@ -331,6 +331,7 @@ class TeamUpdateView(PermissionRequiredMixin, generic.UpdateView):
     form_class = TeamUpdateForm
     url_pattern_name = "task_manager:team_detail"
     permission_required = ("task_manager.view_team", "task_manager.change_team")
+    queryset = Team.objects.exclude_default_team()
 
     def get_success_url(self) -> str:
         return reverse(
