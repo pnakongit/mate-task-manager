96dbd20838d3d30a84de19ff1e156c9aca41d479
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-01-17 21:01:19 +0200

    Add test_templatetags.py with QueryTransformMixin to test

---
+++ task_manager/tests/test_templatetags.py
@@ -0,0 +1,32 @@
+from django.test import RequestFactory
+
+
+class QueryTransformMixin:
+    tag = None
+
+    def setUp(self) -> None:
+        self.factory = RequestFactory()
+
+    def test_without_request_get_parameter(self) -> None:
+        request = self.factory.get("/")
+
+        expected_result = "test=2"
+        self.assertEqual(self.tag(request, test=2), expected_result)
+
+    def test_with_request_get_parameter_and_kwarg_argument(self) -> None:
+        request = self.factory.get("/?first=1")
+
+        expected_result = "first=1&second=2"
+        self.assertEqual(self.tag(request, second=2), expected_result)
+
+    def test_override_existing_get_parameter(self) -> None:
+        request = self.factory.get("/?test=1")
+
+        expected_result = "test=4"
+        self.assertEqual(self.tag(request, test=4), expected_result)
+
+    def test_delete_existing_get_parameter(self) -> None:
+        request = self.factory.get("/?test=1")
+
+        expected_result = ""
+        self.assertEqual(self.tag(request, test=None), expected_result)
