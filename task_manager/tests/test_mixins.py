4eff0cb329e153b890e11e0c1660fd89abadc507
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-01-25 11:43:11 +0200

    Add TaskPermissionRequiredMixinTest to test_mixins.py

--- task_manager/tests/test_mixins.py
+++ task_manager/tests/test_mixins.py
@@ -1,10 +1,11 @@
 from unittest.mock import patch
 
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
 from django.test import TestCase, RequestFactory
 
-from task_manager.mixins import QuerysetFilterByUserMixin
-from task_manager.models import Team, Project
+from task_manager.mixins import QuerysetFilterByUserMixin, TaskPermissionRequiredMixin
+from task_manager.models import Team, Project, Task
 
 
 class QuerysetFilterByUserMixinTest(TestCase):
@@ -73,3 +74,117 @@ class QuerysetFilterByUserMixinTest(TestCase):
             self.mixin_obj.get_queryset()
 
         mock_filter_by_user.assert_called_once()
+
+
+class TaskPermissionRequiredMixinTest(TestCase):
+
+    def setUp(self) -> None:
+        project = Project.objects.create(
+            name="Test project name"
+        )
+        team = Team.objects.create(
+            name="Test team name"
+        )
+        team.projects.add(project)
+        self.task = Task.objects.create(
+            name="Test task name",
+            description="Test descriptions",
+            project=project
+        )
+        user = get_user_model().objects.create_user(
+            username="test_admin",
+            password="123456",
+            email="test@test.com",
+            first_name="Test first name",
+            last_name="Test last name",
+        )
+        request = RequestFactory().request()
+        request.user = user
+
+        mixin_obj = TaskPermissionRequiredMixin()
+        mixin_obj.model = Task
+        mixin_obj.pk_url_kwarg = "pk"
+        mixin_obj.kwargs = {"pk": self.task.pk}
+        mixin_obj.permission_required = "task_manager.view_task"
+        mixin_obj.request = request
+        self.mixin_obj = mixin_obj
+
+    def test_has_permission_return_true_if_user_is_superuser(self) -> None:
+        mixin_obj = self.mixin_obj
+
+        super_user = get_user_model().objects.create_superuser(
+            username="superuser_admin",
+            password="123456",
+            email="testsuoeradmin@test.com",
+            first_name="Test first name",
+            last_name="Test last name"
+        )
+
+        mixin_obj.request.user = super_user
+
+        self.assertTrue(mixin_obj.has_permission())
+
+    def test_has_permission_return_true_if_user_has_permission(self) -> None:
+        mixin_obj = self.mixin_obj
+
+        user = mixin_obj.request.user
+        permission = Permission.objects.get(codename="view_task")
+        user.user_permissions.add(permission)
+
+        self.assertTrue(mixin_obj.has_permission())
+
+    def test_has_permission_return_true_if_user_in_team_of_task_project(self) -> None:
+        mixin_obj = self.mixin_obj
+        team_of_task_project = self.task.project.teams.first()
+
+        user = mixin_obj.request.user
+        user.team = team_of_task_project
+        user.save()
+
+        self.assertFalse(user.get_all_permissions())
+
+        self.assertTrue(self.mixin_obj.has_permission())
+
+    def test_has_permission_return_false_if_user_not_in_team_of_task_project(self) -> None:
+        mixin_obj = self.mixin_obj
+        team_of_task_project = self.task.project.teams.first()
+
+        second_project = Project.objects.create(
+            name="Test second project"
+        )
+        second_team = Team.objects.create(
+            name="Test second team"
+        )
+        second_team.projects.add(second_project)
+
+        user = mixin_obj.request.user
+        user.team = second_team
+        user.save()
+
+        self.assertFalse(user.get_all_permissions())
+        self.assertNotEqual(team_of_task_project, second_team)
+
+        self.assertFalse(self.mixin_obj.has_permission())
+
+    def test_has_permission_return_false_if_obj_not_exist_and_user_doesnt_has_perms(self) -> None:
+        mixin_obj = self.mixin_obj
+        user = mixin_obj.request.user
+
+        self.assertFalse(user.get_all_permissions())
+
+        last_task_pk = Task.objects.last().pk
+        self.mixin_obj.kwargs = {"pk": last_task_pk + 1}
+
+        self.assertFalse(self.mixin_obj.has_permission())
+
+    def test_has_permission_return_true_if_obj_not_exist_and_user_has_perms(self) -> None:
+        mixin_obj = self.mixin_obj
+        user = mixin_obj.request.user
+
+        permission = Permission.objects.get(codename="view_task")
+        user.user_permissions.add(permission)
+
+        last_task_pk = Task.objects.last().pk
+        self.mixin_obj.kwargs = {"pk": last_task_pk + 1}
+
+        self.assertTrue(self.mixin_obj.has_permission())
