7668c662af32c2c9938f76ab5a0a58d9f9687421
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-02-28 20:25:14 +0200

    Add ability to remove page parameter from query in query_transform tag

--- task_manager/templatetags/query_transform.py
+++ task_manager/templatetags/query_transform.py
@@ -1,12 +1,14 @@
 from django import template
+from django.http import HttpRequest
 
 register = template.Library()
 
 
 @register.simple_tag
-def query_transform(request, **kwargs):
+def query_transform(request: HttpRequest, remove_page: bool = False, **kwargs: str) -> str:
     updated = request.GET.copy()
-
+    if remove_page:
+        updated.pop("page", 0)
     for key, value in kwargs.items():
         if value is not None:
             updated[key] = value
