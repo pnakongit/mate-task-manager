324eb10ce0db1ccb709b70dfc8188f44c4faff56
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-03-02 14:32:29 +0200

    Rename project from mate_task_manager  to task_manager_system

--- task_manager_system/asgi.py
+++ task_manager_system/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mate_task_manager project.
+ASGI config for task_manager_system project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mate_task_manager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager_system.settings')
 
 application = get_asgi_application()
