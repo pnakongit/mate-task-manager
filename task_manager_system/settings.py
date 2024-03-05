cfda252d25345103148833e4b41992c0d4e40ace
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-03-05 18:46:34 +0200

    Update SECRET_KEY, DEBUG, ALLOWED_HOSTS in settings.py for deployment

--- task_manager_system/settings.py
+++ task_manager_system/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,12 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g)m&n^)j2p2r!&*he+j$l07u4qd#7rh(k5fm_+#3_fhsh8&ab5'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 # Application definition
 
